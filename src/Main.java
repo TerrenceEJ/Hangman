04a3a72c0adc9b40c6532a7eb33d9c8f06b6903e
TerrenceEJ <terrencejones0820@gmail.com>
2022-04-12 19:06:10 -0400

    update words to use

--- src/Main.java
+++ src/Main.java
@@ -1,106 +1,97 @@
-import javax.swing.text.StyledEditorKit;
-import java.awt.font.TextAttribute;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-
-import static java.awt.font.TextAttribute.*;
+import java.util.*;
 import static java.lang.System.exit;
 
 public class Main {
-    static List<Character> word = Arrays.asList('c', 'a', 't');
+    static List<String> given = new ArrayList<>(6);
+    static List<String> nGiven = new ArrayList<>(1);
+    static List<Character> word = new ArrayList<>();
+
 
     public static void main(String[] args) {
-        ArrayList<Character> prog = new ArrayList<>(word.size());
-        prog.add('_');
-        prog.add('_');
-        prog.add('_');
+        given.add("word");
+        given.add("apple");
+        given.add("free");
+        given.add("helmet");
+        given.add("computer");
+        given.add("programming");
 
+        int chosen = (int) Math.floor(Math.random() * given.size());
+        nGiven.add(given.get(chosen));
+        char[] takeG = nGiven.get(0).toCharArray();
+        ArrayList<Character> prog = new ArrayList<>();
 
-        System.out.println("HANGMAN\n +---+\n \s\s\s\s|\n\s\s\s\s\s|\n\s\s\s\s\s|\n\s\s\s\s\s===\nMissed letters:\n " );
+        for (char i : takeG) {
+            word.add(i);
+            prog.add(i);
+        }
+        System.out.println(prog);
+        for (int i = 0; i < prog.size(); i++) {
+            prog.set(i, '_');
+        }
+
+        System.out.println("HANGMAN\n +---+\n \s\s\s\s|\n\s\s\s\s\s|\n\s\s\s\s\s|\n\s\s\s\s\s===\nMissed letters:\n ");
         System.out.println(change(prog));
+
         Scanner input = new Scanner(System.in);
         int guesses = 0;
 
-        while(!change(prog).equals("cat")) {
+        while (prog.contains('_')) {
             System.out.println("Guess a letter.\n");
             String check = input.next();
-            switch (test(check)) {
-                case ('c'): {
-                    if(prog.get(0) == 'c'){
-                        System.out.println("You've already guessed that letter, try again.");
-                    }else {
-                        prog.set(0, test(check));
-                        System.out.println("Missed letters:\n\n" + change(prog));
+
+            if(prog.contains(test(check))) {
+                System.out.println("You've already used this letter.");
+            }
+            else if (word.contains(test(check))) {
+                System.out.println("Missed letters:\n");
+                prog.set(word.indexOf(test(check)), test(check));
+                prog.set(word.lastIndexOf(test(check)), test(check));
+                System.out.println(change(prog));
+            } else {
+                guesses++;
+                System.out.println("Wrong. Guess again");
+                switch (guesses) {
+                    case (1): {
+                        System.out.println("HANGMAN\n +---+\n O\s\s\s|\n\s\s\s\s\s|\n\s\s\s\s\s|\n\s\s\s\s\s===\nMissed letters:\n ");
+                        break;
                     }
-                    break;
-                }
-                case ('a'): {
-                    if(prog.get(1) == 'a'){
-                        System.out.println("You've already guessed that letter, try again.");
-                    }else {
-                        prog.set(1, test(check));
-                        System.out.println("Missed letters:\n\n" + change(prog));
+                    case (2): {
+                        System.out.println("HANGMAN\n +---+\n O\s\s\s|\n |\s\s\s|\n |\s\s\s|\n\s\s\s===\nMissed letters:\n ");
+                        break;
                     }
-                    break;
-                }
-                case ('t'): {
-                    if(prog.get(2) == 't') {
-                        System.out.println("You've already guessed that letter, try again.");
-                    } else{
-                        prog.set(2, test(check));
-                        System.out.println("Missed letters:\n\n" + change(prog));
+                    case (3): {
+                        System.out.println("HANGMAN\n +---+\n O\s\s\s|\n |\s\s\s|\n |\s\s\s|\n|\s\s\s===\nMissed letters:\n ");
+                        break;
                     }
-                    break;
-                }
-                default:{
-                    guesses++;
-                    System.out.println(guesses);
-                    System.out.println("Wrong. Guess again");
-                    switch (guesses){
-                        case(1):{
-                            System.out.println("HANGMAN\n +---+\n O\s\s\s|\n\s\s\s\s\s|\n\s\s\s\s\s|\n\s\s\s\s\s===\nMissed letters:\n " );
-                            break;
+                    case (4): {
+                        System.out.println("HANGMAN\n +---+\n O\s\s\s|\n |\s\s\s|\n |\s\s\s|\n|\s|===\nMissed letters:\n ");
+                        break;
+                    }
+                    case (5): {
+                        System.out.println("HANGMAN\n +---+\n O\s\s\s|\n" + "/" + "|\s\s\s|\n |\s\s\s|\n|\s|===\nMissed letters:\n ");
+                        break;
+                    }
+                    case (6): {
+                        System.out.println("HANGMAN\n +---+\n O\s\s\s|\n" + "/" + "|\\" + "\s\s|\n |\s\s\s|\n|\s|===\nMissed letters:\n ");
+                        System.out.println("Game Over. \nWould you like to play again (y/n)?");
+                        if (input.next().charAt(0) == 'y') {
+                            main(args);
+                        } else {
+                            exit(0);
                         }
-                        case(2):{
-                            System.out.println("HANGMAN\n +---+\n O\s\s\s|\n |\s\s\s|\n |\s\s\s|\n\s\s\s===\nMissed letters:\n " );
-                            break;
-                        }
-                        case(3):{
-                            System.out.println("HANGMAN\n +---+\n O\s\s\s|\n |\s\s\s|\n |\s\s\s|\n|\s\s\s===\nMissed letters:\n " );
-                            break;
-                        }
-                        case(4):{
-                            System.out.println("HANGMAN\n +---+\n O\s\s\s|\n |\s\s\s|\n |\s\s\s|\n|\s|===\nMissed letters:\n " );
-                            break;
-                        }
-                        case(5):{
-                            System.out.println("HANGMAN\n +---+\n O\s\s\s|\n" + "/" +"|\s\s\s|\n |\s\s\s|\n|\s|===\nMissed letters:\n " );
-                            break;
-                        }
-                        case(6):{
-                            System.out.println("HANGMAN\n +---+\n O\s\s\s|\n" + "/" + "|\\" + "\s\s|\n |\s\s\s|\n|\s|===\nMissed letters:\n " );
-                            System.out.println("Game Over \n Would you like to play again (y/n)?");
-                            if(input.next().charAt(0) == 'y'){
-                                main(args);
-                            } else{
-                                exit(0);
-                            }
-                            break;
-                        }
-                        default:{
-                            System.out.println("error");
-                            break;
-                        }
+                        break;
+                    }
+                    default: {
+                        System.out.println("error");
+                        break;
                     }
                 }
             }
-       }
-        System.out.println("Yes! The secret word is \"cat!\"" + "Would you like to play again (y/n)?");
-        if(input.next().charAt(0) == 'y'){
+        }
+        System.out.println("Yes! The secret word is " + change(prog) + "! Would you like to play again (y/n)?");
+        if (input.next().charAt(0) == 'y') {
             main(args);
-        } else{
+        } else {
             exit(0);
         }
     }
